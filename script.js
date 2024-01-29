9c4449f34c48b964752eaef4bc28aa84e890246e
Yaseenjabir <yaseenkhan1235476@gmail.com>
2024-01-29 15:52:03 +0000

    File Updated

---
+++ script.js
@@ -0,0 +1,23 @@
+const display = document.querySelector("input");
+const buttons = document.querySelectorAll("button");
+const buttonsArray = Array.from(buttons);
+
+let string = "";
+
+buttonsArray.forEach((elements) => {
+  elements.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "DEL") {
+      string = string.substring(0, string.length - 1);
+      display.value = string;
+    }else if(e.target.innerHTML === "AC"){
+      string = '';
+      display.value = string;
+    }else if(e.target.innerHTML === "="){
+      string = eval(string);
+      display.value = string;
+    }else {
+      string += e.target.innerHTML;
+      display.value = string;
+    }
+  });
+});
